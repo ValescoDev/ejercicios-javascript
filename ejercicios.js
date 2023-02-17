53005c82eac3c7979cf4a471324857248e0b0431
ValescoDev <gilmolsv@gmail.com>
2023-02-17 16:03:23 -0600

    estos son los primeros ejercicios

---
+++ ejercicios.js
@@ -0,0 +1,66 @@
+// Reto 03 
+// crear el sistema de calendario para verificar si el día actual, junto a la hora es considerada día y horario laboral estándar: 9am - 6pm es horario laboral. usa new Date().getDay() resulta en el día de la semana del 0 - 6. Función businessHours. Parametros: hour = hora actual. Return: True/False si la hora actual es horario laboral. Función businessDay. parametros: day. Return: True/False si el día es laboral. Verificación de la resolucion de ambas funciones debe mostrar un mensaje.
+
+const businessHours = (hour) => hour >= 9 && hour <= 18;
+const businessDay = (day) => day >= 1 && day <= 5;
+const isBusinessHours = businessDay(new Date().getDay()) && businessHours(new Date().getHours());
+
+// Display on the console
+console.log(isBusinessHours ? "La hora actual marca que es horario laboral" : "La hora actual marca que no es horario laboral");
+
+////////////////////////////////////////////////////////////////////////
+
+// Ejercicio 04
+// Escribir una funcion que regrese la cantidad de valores true que hay en un array
+/* 
+countTrue([true, false, false, true, false]) ➞ 2
+
+countTrue([false, false, false, false]) ➞ 0
+
+countTrue([]) ➞ 0
+ */
+// Regresar 0 si el array es vacio
+// usar recurcion
+
+const countTrue = (arr) => arr.length === 0 ? 0 : (arr[0] ? 1 : 0) + countTrue(arr.slice(1));
+
+// Example of use
+const arr = [true, false, false, true, false];
+//const arr = [false, false, false, false, false]; //➞ 0
+//const arr = []; //➞ 0
+
+// Display 2 on the console
+console.log(countTrue(arr)); 
+
+////////////////////////////////////////////////////////////////////////
+
+//Ejercicio 05
+// Escribir una funcion para encontrar el mayor comun divisor de 2 numeros positivos enteros
+// Usar recursion
+
+const greatestCommonDivisor = (a, b) => b === 0 ? a : greatestCommonDivisor(b, a % b);
+
+// Example of use
+// The result should be 14
+console.log(greatestCommonDivisor(14, 56));
+
+
+// Encontrar los primeros n elementos de la serie fibonacci
+// Usar recursion 
+
+const fi = (n) => {
+   if( n === 0){
+    return [];
+} else if (n === 1) {
+    return [0];
+} else if (n === 2) {
+    return [0, 1];
+} else {
+const array = fi(n - 1);
+    array.push(array[array.length - 1] + array[array.length - 2])
+    return array; } }
+
+// Example of use
+// The result should be [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+console.log(fi(10));
+
